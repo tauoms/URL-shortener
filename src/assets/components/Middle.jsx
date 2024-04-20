9cb03205d22b1a5ca125fe9027d844d1ac128d8c
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-20 19:11:47 +0300

    Install Axios, create handleChange and handleSubmit functions

--- src/assets/components/Middle.jsx
+++ src/assets/components/Middle.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Middle = () => {
+  const [inputUrl, setInputUrl] = useState("");
+
+  const handleChange = (event) => {
+    const longUrl = event.target.name;
+    const value = event.target.value;
+    setInputUrl((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:8005/api/", inputUrl);
+    // console.log(inputUrl);
+  };
+
   return (
     <section>
-      <form method="post">
+      <form onSubmit={handleSubmit}>
         <label for="longUrl">Enter URL to shorten:</label>
         <br />
-        <input type="text" id="longUrl" name="longUrl" required />
+        <input
+          type="text"
+          id="longUrl"
+          name="longUrl"
+          required
+          onChange={handleChange}
+        />
         <br />
         <button type="submit">Shorten URL</button>
       </form>
