24628303b65ad909832a2d048308f85ba44badee
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-20 18:21:13 +0300

    Style page

--- src/assets/components/Middle.jsx
+++ src/assets/components/Middle.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Middle = () => {
-  return <section>Middle</section>;
+  return (
+    <section>
+      <form method="post">
+        <label for="longUrl">Enter URL to shorten:</label>
+        <br />
+        <input type="text" id="longUrl" name="longUrl" required />
+        <br />
+        <button type="submit">Shorten URL</button>
+      </form>
+    </section>
+  );
 };
 
 export default Middle;
