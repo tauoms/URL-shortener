b1b40aec71cb41e7ee33ac8a10ce3cec1e82d139
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-21 19:53:54 +0300

    Add link to unelma io, adjust flex alignment of main elements

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer>
       &copy; 2024 Tuomas Kohvakka <br />
-      API by Unelma
+      API by <a href="https://unelma.io/">Unelma.IO</a>
     </footer>
   );
 };
