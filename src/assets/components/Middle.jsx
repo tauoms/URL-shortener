0f95db20a84bff31fecbd2d3ee7a4fd9cefc91b7
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-21 14:57:44 +0300

    Trying to read api key from .env, getting error process is not defined

--- src/assets/components/Middle.jsx
+++ src/assets/components/Middle.jsx
@@ -2,36 +2,59 @@ import { useState } from "react";
 import axios from "axios";
 
 const Middle = () => {
+  const apiKey = process.env.REACT_APP_API_TOKEN;
   const [inputUrl, setInputUrl] = useState("");
+  const [dataForApi, setDataForApi] = useState({
+    type: "direct",
+    password: apiKey,
+    active: true,
+    expires_at: "2024-05-06",
+    activates_at: "2024-03-25",
+    utm: "utm_source=google&utm_medium=banner",
+    domain_id: null,
+    long_url: "",
+  });
 
   const handleChange = (event) => {
-    const longUrl = event.target.name;
     const value = event.target.value;
-    setInputUrl((values) => ({ ...values, [name]: value }));
+    setInputUrl(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post("http://localhost:8005/api/", inputUrl);
-    // console.log(inputUrl);
+    setDataForApi({ ...dataForApi, long_url: inputUrl });
+    axios
+      .post("https://unelma.io/api/v1/link/", dataForApi)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log(dataForApi);
+    console.log(inputUrl);
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <label for="longUrl">Enter URL to shorten:</label>
-        <br />
-        <input
-          type="text"
-          id="longUrl"
-          name="longUrl"
-          required
-          onChange={handleChange}
-        />
-        <br />
-        <button type="submit">Shorten URL</button>
-      </form>
-    </section>
+    <>
+      <p id="result">{inputUrl}</p>
+
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="longUrl">Enter URL to shorten:</label>
+          <br />
+          <input
+            type="text"
+            id="longUrl"
+            name="longUrl"
+            required
+            onChange={handleChange}
+          />
+          <br />
+          <button type="submit">Shorten URL</button>
+        </form>
+      </section>
+    </>
   );
 };
 
